c0c2cc22a107a3ea7132d310786b0efad7470e91
Michael Frieze <michaelfrieze@icloud.com>
2024-10-06 16:14:23 -0400

    add comment about stoker to app

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+// I am using notFound handler from stoker
+// I added a stoker directory and copied the stoker code to my project
+// https://github.com/w3cj/stoker/blob/main/src/middlewares/not-found.ts
+// You can also: npm install stoker
 app.notFound(notFound);
 
 export default app;
