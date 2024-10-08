21d353dbe758acf1aec7a95551971d57d77d34f5
Michael Frieze <michaelfrieze@icloud.com>
2024-10-07 21:38:18 -0400

    add hono-pino and pino-pretty for logging

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 
 import { notFound, onError } from "../stoker/middlewares";
+import { pinoLogger } from "./middlewares/pino-logger";
 
 const app = new OpenAPIHono({});
+app.use(pinoLogger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
